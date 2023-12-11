de0388ce57bae06368d6a4756f34ce3c1a9380ea
elpollitodiablo <daniel@thrash-media.com>
2023-12-11 22:09:40 +0100

    feat: updates and .... things

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const fs = require('fs');
-const uglifycss = require('gulp-uglifycss');
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create()
+const fs = require('fs')
+const uglifycss = require('gulp-uglifycss')
 
 const src = {
     scss: 'src/scss/**/*.scss',
@@ -11,7 +11,7 @@ const src = {
     css: 'src/css',
     html: 'src/*.html',
     env: 'src/site.env',
-};
+}
 
 const sassCompile = (cb) => {
     gulp.src(src.scss, { sourcemaps: true })
@@ -28,32 +28,37 @@ const sassCompile = (cb) => {
             })
         )
         .pipe(gulp.dest(src.css, { sourcemaps: '.' }))
-        .pipe(browserSync.stream());
+        .pipe(browserSync.stream())
 
-    cb();
-};
+    cb()
+}
 
 gulp.task('copyFontAwesomeFonts', () => {
-    return gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*').pipe(gulp.dest('./src/css/webfonts'));
-});
+    return gulp
+        .src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('./src/css/webfonts'))
+})
 
 gulp.task('sass', (cb) => {
-    return sassCompile(cb);
-});
+    return sassCompile(cb)
+})
 
 gulp.task('setTargetEnvironment', (cb) => {
-    const params = process.argv.filter((a) => a.startsWith('--target='));
-    if (params.length !== 1) throw new Error('Task "serve" must be followed by the parameter "--target=xyz"');
+    const params = process.argv.filter((a) => a.startsWith('--target='))
+    if (params.length !== 1)
+        throw new Error(
+            'Task "serve" must be followed by the parameter "--target=xyz"'
+        )
 
-    const param = params[0];
-    let targetEnv = param.split('=')[1];
-    if (!targetEnv) targetEnv = 'local';
-    targetEnv = targetEnv.toLowerCase();
+    const param = params[0]
+    let targetEnv = param.split('=')[1]
+    if (!targetEnv) targetEnv = 'local'
+    targetEnv = targetEnv.toLowerCase()
 
-    console.log(`Use target environment ${targetEnv}`);
+    console.log(`Use target environment ${targetEnv}`)
 
-    fs.copyFile(`./env/${targetEnv}.properties`, src.env, cb);
-});
+    fs.copyFile(`./env/${targetEnv}.properties`, src.env, cb)
+})
 
 gulp.task(
     'serve',
@@ -65,8 +70,8 @@ gulp.task(
             },
             port: 3000,
             open: false,
-        });
-        gulp.watch(src.js).on('change', browserSync.reload);
-        gulp.watch(src.scss, sassCompile);
+        })
+        gulp.watch(src.js).on('change', browserSync.reload)
+        gulp.watch(src.scss, sassCompile)
     })
-);
+)
--- src/js/script.js
+++ src/js/script.js
@@ -1,75 +1,97 @@
-import { RedditTopOfTheDayComponent } from './RedditTopOfTheDayComponent.js';
+import { RedditTopOfTheDayComponent } from './RedditTopOfTheDayComponent.js'
 
-RedditTopOfTheDayComponent.register();
+RedditTopOfTheDayComponent.register()
 
-const sections = document.querySelectorAll('.section-content');
-const covers = document.querySelectorAll('.section-cover');
-let coversLoaded = 0;
+const sections = document.querySelectorAll('.section-content')
+const covers = document.querySelectorAll('.section-cover')
+let coversLoaded = 0
+
+const p = [176, 61, 88, 25, 27]
+
+const m = ['d', 'an', 'ie', 'l', '@', 'so', 't', 'z', 'ko', '.', 'net']
+
+document.querySelector('#phone-revealer').addEventListener('click', () => {
+    const phone = document.querySelector('#phone')
+    phone.attributes.href.value = `tel:+49${p.join('')}`
+    phone.innerHTML = `+49 ${p.join(' ')}`
+})
+
+document.querySelector('#email-revealer').addEventListener('click', () => {
+    const phone = document.querySelector('#email')
+    phone.attributes.href.value = `mailto:${m.join('')}`
+    phone.innerHTML = `${m.join('')}`
+})
+
+setTimeout(() => {
+    document.querySelector('#hi-there').classList.add('on')
+}, 500)
 
 const throttle = (fn, wait) => {
-    let inThrottle, lastFn, lastTime;
+    let inThrottle, lastFn, lastTime
     return function () {
         const context = this,
-            args = arguments;
+            args = arguments
         if (!inThrottle) {
-            fn.apply(context, args);
-            lastTime = Date.now();
-            inThrottle = true;
+            fn.apply(context, args)
+            lastTime = Date.now()
+            inThrottle = true
         } else {
-            clearTimeout(lastFn);
+            clearTimeout(lastFn)
             lastFn = setTimeout(function () {
                 if (Date.now() - lastTime >= wait) {
-                    fn.apply(context, args);
-                    lastTime = Date.now();
+                    fn.apply(context, args)
+                    lastTime = Date.now()
                 }
-            }, Math.max(wait - (Date.now() - lastTime), 0));
+            }, Math.max(wait - (Date.now() - lastTime), 0))
         }
-    };
-};
+    }
+}
 
 const ckeckItemInView = (element) => {
-    const pos = element.getBoundingClientRect();
+    const pos = element.getBoundingClientRect()
     if (window.scrollY > pos.bottom && element.classList.contains('off')) {
-        element.classList.remove('off');
+        element.classList.remove('off')
     }
-};
+}
 
 const onImageLoaded = (e) => {
-    coversLoaded++;
+    coversLoaded++
     if (coversLoaded === covers.length) {
-        document.querySelector('#splash-container .strp-grid').classList.add('off');
+        document
+            .querySelector('#splash-container .strp-grid')
+            .classList.add('off')
         setTimeout(() => {
-            document.querySelector('#splash-container').classList.add('off');
-        }, 5000);
+            document.querySelector('#splash-container').classList.add('off')
+        }, 5000)
     }
-};
+}
 
 document.addEventListener(
     'scroll',
     throttle((e) => {
-        sections.forEach((element) => ckeckItemInView(element));
+        sections.forEach((element) => ckeckItemInView(element))
 
-        document.querySelector('#splash-container').classList.add('off');
+        document.querySelector('#splash-container').classList.add('off')
     }, 100)
-);
-sections.forEach((element) => ckeckItemInView(element));
+)
+sections.forEach((element) => ckeckItemInView(element))
 
 covers.forEach((cover) => {
-    const css = window.getComputedStyle(cover, ':after');
-    const re = /url\("(.+)"\)/;
-    const imageMatch = css.backgroundImage.match(re);
+    const css = window.getComputedStyle(cover, ':after')
+    const re = /url\("(.+)"\)/
+    const imageMatch = css.backgroundImage.match(re)
 
     if (imageMatch && imageMatch[1]) {
-        const img = new Image();
-        img.src = imageMatch[1];
+        const img = new Image()
+        img.src = imageMatch[1]
         img.onload = (e) => {
-            onImageLoaded(e);
-        };
+            onImageLoaded(e)
+        }
         img.onerror = (e) => {
-            onImageLoaded(e);
-        };
+            onImageLoaded(e)
+        }
     } else {
-        onImageLoaded();
-        coversLoaded++;
+        onImageLoaded()
+        coversLoaded++
     }
-});
+})
